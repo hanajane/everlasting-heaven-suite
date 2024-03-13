68c0497afcbf7ed50f0bd284ba300a63251939f9
Hanajane <programmingcollections@gmail.com>
2024-03-13 17:50:00 -0400

    configure routes for testing

---
+++ resources/views/greeting.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <!-- Act only according to that maxim whereby you can, at the same time, will that it should become a universal law. - Immanuel Kant -->
+
+<h1>Greeting page
+</h1>
+</div>
+
+
